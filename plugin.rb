37fe7483429209ea139769fc0f17b6a11ce1d985
Riccardo T <riccardo.terzaghi1@gmail.com>
2022-10-28 12:41:32 +0100

    google mapping

--- plugin.rb
+++ plugin.rb
@@ -5,3 +5,9 @@
 # url: https://github.com/richterzo/discourse-uzbek
 
 register_locale("uz", name: "Uzbek", nativeName: "Ўзбек", fallbackLocale: "en_GB")
+
+after_initialize do
+    if defined? DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING
+      DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING[:uz] = "uz"
+    end
+  end
